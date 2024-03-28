eb35eb4009072097fe8ca2b48bc6f5ac8de40e57
Robert Leeuwerink <rleeuwerink@gmail.com>
2024-03-27 17:08:22 -0700

    fix: build error

--- hosting/app/(pages)/contactus/page.tsx
+++ hosting/app/(pages)/contactus/page.tsx
@@ -3,27 +3,26 @@ import Script from "next/script";
 declare const window: any;
 
 export default function ContactUs() {
+  const hubspot = () => {
+    if (window.hbspt) {
+      window.hbspt.forms.create({
+        region: "na1",
+        portalId: "6868694",
+        formId: "c27e0357-f5de-455d-80bb-1c8387b145f0",
+        target: "#hubspotForm",
+      });
+    }
+  };
   return (
-    <div className="content block">      
-      <Script src="https://js.hsforms.net/forms/v2.js" onLoad={hubspot}
-        strategy="lazyOnload" />
-      <div
-        id="contactus"
-      >
+    <div className="content block">
+      <Script
+        src="https://js.hsforms.net/forms/v2.js"
+        onLoad={hubspot}
+        strategy="lazyOnload"
+      />
+      <div id="contactus">
         <div id="hubspotForm"></div>
       </div>
     </div>
   );
-}
-
-export const hubspot = () => LoadHubspotForm();
-export function LoadHubspotForm() {
-  if(window.hbspt) {
-    window.hbspt.forms.create({
-      region: "na1",
-      portalId: "6868694",
-      formId: "c27e0357-f5de-455d-80bb-1c8387b145f0",
-      target: '#hubspotForm'
-    });
- }
 }
