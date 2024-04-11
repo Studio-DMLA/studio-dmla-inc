9a80e4250013bf9476f4fba247333f327f28a73f
Robert Leeuwerink <rleeuwerink@gmail.com>
2024-04-11 10:31:20 -0700

    github actions

--- hosting/app/components/snippets/section-splitter.tsx
+++ hosting/app/components/snippets/section-splitter.tsx
@@ -8,8 +8,10 @@ interface Props {
 
 export default function SectionSplitter(props: Props) {
   const offset = props.offset || 0;
-  const clipping = `polygon(0 0, ${90 + offset}% 0, 100% 100%, 0% 100%)`;
-  return (<div className={`bg-color_${props.endColor} full`} style={{minHeight: '1rem', maxHeight: '3rem', height: '3vw'}}>
-            <div className={`bg-color_${props.startColor} full`} style={{clipPath: clipping, minHeight: '1rem', maxHeight: '3rem', height: '3vw', width: `${props.split}%`, marginTop: '1px'}}></div>
+  const clipping = `polygon( 0 0, 100% 0%, 100% 100%, ${5 + offset}% 100%)`;
+  return (<div className={`bg-color_${props.startColor} full subgrid`} style={{minHeight: '1rem', maxHeight: '3rem', height: '3vw', display: 'grid', gridTemplateColumns: `${props.split/2}% ${props.split/2}% 1fr`, marginBottom: '-3px'}}>
+            <div className={`bg-color_${props.startColor}`}></div>
+            <div className={`bg-color_${props.startColor}`} ></div>
+            <div className={`bg-color_${props.endColor}`}  style={{clipPath: clipping, marginTop: '-3px'}}></div>
           </div>);
 }
